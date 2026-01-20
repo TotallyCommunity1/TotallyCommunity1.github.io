dbc31588ed77f9d295e4d81fea5d699902531eff
TotallyCommunity1 <hplay5933@gmail.com>
2026-01-20 18:38:38 -0500

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -4,23 +4,28 @@ const channel2 = "UCLbcla_6HSJYBIyMhf_Osag";
 function loadVideos(channelId, containerId) {
   const rss = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`;
 
-  fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(rss)}`)
-    .then(res => res.json())
-    .then(data => {
+  fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(rss)}`)
+    .then(res => res.text())
+    .then(str => {
       const parser = new DOMParser();
-      const xml = parser.parseFromString(data.contents, "text/xml");
+      const xml = parser.parseFromString(str, "text/xml");
       const entries = xml.querySelectorAll("entry");
 
       let html = "";
-      for (let i = 0; i < 3; i++) {
+      for (let i = 0; i < Math.min(3, entries.length); i++) {
         const videoId = entries[i].querySelector("yt\\:videoId").textContent;
         html += `
-          <iframe src="https://www.youtube.com/embed/${videoId}" allowfullscreen></iframe>
+          <iframe 
+            src="https://www.youtube.com/embed/${videoId}" 
+            frameborder="0" 
+            allowfullscreen>
+          </iframe>
         `;
       }
 
       document.getElementById(containerId).innerHTML = html;
-    });
+    })
+    .catch(err => console.error(err));
 }
 
 loadVideos(channel1, "channel1");
